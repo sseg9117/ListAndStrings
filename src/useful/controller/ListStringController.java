9525881296b352a13913a44c186e64012eaf26ab
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-10-03 08:57:13 -0600

    Added comments to everything to exsplain what it is.

--- src/useful/controller/ListStringController.java
+++ src/useful/controller/ListStringController.java
@@ -1,13 +1,22 @@
 package useful.controller;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.List;// imports List
+import java.util.ArrayList;// imports ArrayList
+import useful.model.Donut;
 
 public class ListStringController
 {
-
-		public void start()
+	private List<Donut> donutList; //This declares the list
+	
+	public ListStringController()
+	{
+		donutList = new ArrayList<Donut>();// This initializes the list
+	}
+	
+		public void start() // allows you to start the program
 		{
-			
+			Donut temp = new Donut();//makes temp instanse of donut / makes donut object
+//			System.out.println(donutList);
+			donutList.add(temp); // you can put temp here instead of new donut because of ^
 		}
 }
